80c6f2a7cf93265e9c9ae58569e74dbf1992725b
Babc8161 <babc8161@yahoo.com>
2022-02-15 00:59:25 -0500

    mvp of project

---
+++ main.py
@@ -0,0 +1,45 @@
+from tkinter import *
+import webbrowser
+
+# def open_entry():
+#     """Opens the text written by user"""
+#     webbrowser.open("myJournal.txt")
+
+def read_entry():
+
+    with open("myJournal.txt", "r") as f:
+        print(f.read())
+
+def log_entry():
+    journal_entry = textbox.get()
+    journal_entry = journal_entry + "\n"
+    textbox.delete(0, END)
+    with open("myJournal.txt", "a") as o:
+        o.write(journal_entry)
+
+def exit():
+    root.destroy()
+
+root = Tk()
+root.geometry("500x400")
+root.title("myJournal")
+
+menubar = Menu(root)
+
+options = Menu(menubar, tearoff=0)
+
+options.add_command(label="Past Entries", command=read_entry)
+options.add_command(label="Exit", command=exit)
+
+menubar.add_cascade(label="Options", menu=options)
+root.config(menu=menubar)
+
+journal = Label(root, text="Log an Entry")
+journal.pack()
+textbox = Entry(root, bd=5, width=80)
+textbox.pack()
+save = Button(text="Save", command=log_entry)
+save.pack()
+
+root.mainloop()
+
